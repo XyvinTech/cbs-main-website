f582cd13d70a4fb3d2b4b8927857f847cf21d762
Fasilfaz <fazsby2001@gmail.com>
2025-03-13 10:34:50 +0530

    mobile menu z-index issue fixed

--- components/layout/Navbar/index.tsx
+++ components/layout/Navbar/index.tsx
@@ -13,7 +13,7 @@ export default function Navbar() {
       <Container>
         <div className="flex items-center justify-between relative">
           {/* Logo */}
-          <Link href="/" className="logo relative z-[101] cursor-pointer flex flex-row">
+          <Link href="/" className="logo relative  cursor-pointer flex flex-row">
             <Image
                 src='/images/logo.png'
                 alt="logo"
